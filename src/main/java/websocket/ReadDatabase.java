d09a2bd2672e85f7f3d47378a689bd4060a0a3a5
systemad <dannesuke@gmail.com>
2021-01-19 14:18:10 +0100

    revers commits

--- src/main/java/websocket/ReadDatabase.java
+++ src/main/java/websocket/ReadDatabase.java
@@ -1,42 +1,34 @@
-package Testing;
-
+package websocket;
 import javax.websocket.Session;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
-public class fetchDatabase {
-    public static void main(String[] args, Session session) throws FileNotFoundException, IOException {
-
+public class ReadDatabase {
+    Properties p = new Properties();
+    public ReadDatabase(Session session) throws IOException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         }catch (ClassNotFoundException e){
             e.printStackTrace();
         }
-
-        Properties p = new Properties();
-        p.load(new FileInputStream("C:/Users/Dan/IdeaProjects/WebSocketIntelliJAA/src/websocket/settings.properties"));
-
+        p.load(new FileInputStream("C:\\Users\\Dan\\IdeaProjects\\WebSocketIntelliJ\\src\\main\\java\\websocket\\settings.properties"));
         try(Connection con = DriverManager.getConnection(
                 p.getProperty("connectionString"),
                 p.getProperty("name"),
                 p.getProperty("password"));
-
             Statement stmt = con.createStatement();
-                                                    // Order by time
+            // Order by time
             ResultSet rs = stmt.executeQuery("SELECT * FROM data ORDER BY ID DESC LIMIT 1")) {
 
             while (rs.next()) {
-                //int id = rs.getInt("id"); // BYT till tid
-                float temperature = rs.getInt("temperature");
+                int temperature = rs.getInt("temperature");
                 int humidity = rs.getInt("humidity");
-
                 session.getBasicRemote().sendText(temperature + " " + humidity);
             }
         }
-        catch(SQLException e ){
+        catch(SQLException | IOException e ){
             e.printStackTrace();
         }
     }
