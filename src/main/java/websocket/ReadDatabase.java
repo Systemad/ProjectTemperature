3bc22243fe7a1e8208ccacb83624a5452545fadf
systemad <dannesuke@gmail.com>
2021-01-19 01:19:14 +0100

    ReadDatabase: cleanup

--- src/main/java/websocket/ReadDatabase.java
+++ src/main/java/websocket/ReadDatabase.java
@@ -1,28 +1,23 @@
 package websocket;
 import javax.websocket.Session;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
 public class ReadDatabase {
-
+    Properties p = new Properties();
     public ReadDatabase(Session session) throws IOException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         }catch (ClassNotFoundException e){
             e.printStackTrace();
         }
-
-        Properties p = new Properties();
         p.load(new FileInputStream("C:/Users/Dan/IdeaProjects/WebSocketIntelliJAA/src/websocket/settings.properties"));
-
         try(Connection con = DriverManager.getConnection(
                 p.getProperty("connectionString"),
                 p.getProperty("name"),
                 p.getProperty("password"));
-
             Statement stmt = con.createStatement();
             // Order by time
             ResultSet rs = stmt.executeQuery("SELECT * FROM data ORDER BY ID DESC LIMIT 1")) {
